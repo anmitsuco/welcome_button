7341362a4bb64536e866df020cefeeb4e82b1845
anmitsuco <katoyoshiko.15@gmail.com>
2025-04-16 16:34:27 +0900

    ファイルを差し替えました

--- js/script.js
+++ js/script.js
@@ -9,21 +9,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const isShown = container.classList.contains("show");
 
     if (isShown) {
-      // 非表示にする
       container.classList.remove("show");
-      setTimeout(() => {
-        container.style.display = "none";
-      }, 300); // アニメーションがある場合は少し待つ
     } else {
-      // 表示にする
-      container.style.display = "block";
-      setTimeout(() => {
-        container.classList.add("show");
-      }, 10);
+      container.classList.add("show");
 
       const containerHeight = container.offsetHeight;
       window.scrollBy({
-        top: containerHeight - 100,
+        top: containerHeight - 80,
         behavior: "smooth"
       });
     }
